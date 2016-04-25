62d6c88338f8b48d4029a9ee2a3f95c3959060c9
denonbarton <dbar0540@740s-pl-126.local>
2016-04-25 12:09:10 -0600

    ,

---
+++ model/CTECGraph.cpp
@@ -0,0 +1,42 @@
+//
+//  CTECGraph.cpp
+//  XCodeNodes
+//
+//  Created by Barton, Denon on 4/25/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef CTECGraph_hpp
+#define CTECGraph_hpp
+#include <set>
+
+namespace CTECData
+{
+    template <class Type>
+    class CTECGraph
+    {
+    private:
+        static const int MAXIMUM = 20;
+        bool adjacencyMatrix[MAXIMUM] [MAXIMUM];
+        Type labels[MAXIMUM];
+        int manyVertices;
+        
+    public:
+        CTECGraph();
+        ~CTECGraph();
+        
+        void addVertex(const Type& value);
+        void addEdge(int source, int target);
+        void removdge(int source, int target);
+        Type& operator [] (int vertex);
+        Type operator [] (int vertex) const;
+        int size();
+        bool lsEdge(int source, int target) const;
+        std::set<int> neighbors(int vertex) const;
+        
+        
+        
+        
+    };
+}
+#endif /* CTECGraph_hpp*/
---
+++ model/CTECGraph.hpp
@@ -0,0 +1,14 @@
+//
+//  CTECGraph.hpp
+//  XCodeNodes
+//
+//  Created by Barton, Denon on 4/25/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef CTECGraph_hpp
+#define CTECGraph_hpp
+
+#include <stdio.h>
+
+#endif /* CTECGraph_hpp */
