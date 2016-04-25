5d7b1464379b1b66d2aac82e6ae6bff8f90267ce
denonbarton <dbar0540@740s-pl-126.local>
2016-04-25 12:35:54 -0600

    123

--- model/CTECGraph.cpp
+++ model/CTECGraph.cpp
@@ -33,5 +33,40 @@ void CTECGraph<Type> :: addVertex(const Type&value)
 template <class Type>
 void CTECGraph<Type> :: addEdge(int source, int target)
 {
-    
+    assert(source <size() && target < size());
+    adjacencyMatrix[source][target] = true;
 }
+
+template <class Type>
+    bool CTECGraph<Type> :: isEdge(int source, int target) const
+{
+    assert(source < size() && target< size());
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target];
+    
+    return isAnEdge;
+}
+
+template <class Type>
+    Type& CTECGph<Type> :: operator[](int vertex) const
+    {
+        assert(vertex < size());
+        return labels[vertex];
+    }
+
+template <class Type>
+    std::set<int> CTECGraph<Type> :: neighbors(int vertex) const
+    {
+        assert(vertex < size());
+        std::set<int> vertexNeibors;
+        
+        for(int index = 0; index <size(); index++)
+        {
+            if(adjacencuMatrix[vertex][index])
+            {
+                vertexNeighbors.insert(index);
+            }
+        }
+        return vertexNeighbors;
+    }
