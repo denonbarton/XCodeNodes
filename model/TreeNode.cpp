ce992858785a234b85e28934f8ba62bbdfdd8751
denonbarton <dbar0540@740s-pl-126.local>
2016-05-24 12:59:53 -0600

    fixed code

---
+++ model/TreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  TreeNode.cpp
+//  XCodeNodes
+//
+//  Created by Barton, Denon on 5/23/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "TreeNode.hpp"
---
+++ model/TreeNode.h
@@ -0,0 +1,14 @@
+//
+//  TreeNode.hpp
+//  XCodeNodes
+//
+//  Created by Barton, Denon on 5/23/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
+
+#include <stdio.h>
+
+#endif /* TreeNode_hpp */
